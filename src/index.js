c2b8a085dc42fc56d524b36625290c66c0ef9d69
keitorita <77565939+keitorita@users.noreply.github.com>
2021-02-07 12:56:11 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -22,15 +22,22 @@
 // letは再宣言不可能
 // let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
 // const変数は上書き不可
 // val3="const変数を上書き";
-// const val3="const変数を再宣言";
-
-const val4 = {
-  name: "かわかみ",
-  age: 28
-};
-console.log(val4);
+// const val3="const変数を再宣言不可";
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "かわかみ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "hiroshima";
+// console.log(val4);
+// constで定義した配列の変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
